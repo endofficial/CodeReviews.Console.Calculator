0996b512381b74290f1b6dc8e4087d568f387769
Andrea Verdirrame <andreaverdirrame11@gmail.com>
2025-12-28 17:30:13 +0100

    Project finished!

---
+++ Calculator#2/Calculator/Calculator/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Text.RegularExpressions;
+using CalculatorLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool endApp = false;
+        WriteLine("Console Calculator in C#\r"); 
+        WriteLine("------------------------\n");
+
+        Calculator calculator = new Calculator();
+
+        while (!endApp)
+        {
+            string? numInput1 = "";
+            string? numInput2 = "";
+            double result = 0;
+
+            Write("Type a number, and then press Enter: ");
+            numInput1 = ReadLine();
+
+            Write("Type another number, and then press Enter: ");
+            numInput2 = ReadLine();
+
+            double cleanNum1 = 0;
+            while (!double.TryParse(numInput1, out cleanNum1))
+            {
+                Write("This is not valid input. Please enter an integer value: ");
+                numInput1 = ReadLine();
+            }
+
+            double cleanNum2 = 0;
+            while (!double.TryParse(numInput2, out cleanNum2))
+            {
+                Write("This is not valid input. Please enter an integer value: ");
+                numInput2 = ReadLine();
+            }
+
+            WriteLine("Choose an operator from the following list:");
+            WriteLine("\ta - Add");
+            WriteLine("\ts - Subtract");
+            WriteLine("\tm - Multiply");
+            WriteLine("\td - Divide");
+            WriteLine("Your option? ");
+
+            string? op = ReadLine();
+            if (op is null || !Regex.IsMatch(op, "[a|s|m|d]"))
+            {
+                WriteLine("Invalid option. Please select a valid operator.");
+            }
+            else
+            {
+                try
+                {
+                    result = calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        WriteLine("This operation will result in a mathematical error.\n");
+                    }
+                    else
+                    {
+                        WriteLine("Your result: {0:0.##}\n", result); // Format to 2 decimal places
+                    }
+                }
+                catch (Exception e)
+                {
+                    WriteLine("Oh no! An exception occurred trying to do the math.\\n - Details: " + e.Message);
+                }
+            }
+            WriteLine("------------------------\n");
+
+            Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+            if (ReadLine() is "n")
+            {
+                endApp = true;
+            }
+        }
+        calculator.Finish();
+        return;
+    }
+}
---
+++ Calculator#2/Calculator/CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        JsonWriter writer;
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented; // indica come deve essere formattato l'output di testo JSON
+            writer.WriteStartObject(); // scrive l'inzio di un oggetto JSON
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+
+            /*StreamWriter logFile = File.CreateText("calculator.log");
+            Trace.Listeners.Add(new TextWriterTraceListener(logFile)); // ottieni la raccolta di listener che esegue il monitoraggio dell'output di traccia
+            Trace.AutoFlush = true; // indica se chiamare il metodo flush() sulla proprietà listeners dopo ogni operazione di scrittura
+            Trace.WriteLine("Starting Calculator Log");
+            Trace.WriteLine(String.Format("Started {0}", System.DateTime.Now.ToString()));*/
+        }
+
+        public double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; // Default value is "not-a-number"
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operation");
+
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    writer.WriteValue("Add");
+                    //Trace.WriteLine(String.Format("{0} + {1} = {2}", num1, num2, result));
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    writer.WriteValue("Subtract");
+                    //Trace.WriteLine(String.Format("{0} - {1} = {2}", num1, num2, result));
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    writer.WriteValue("Multiply");
+                    //Trace.WriteLine(String.Format("{0} * {1} = {2}", num1, num2, result));
+                    break;
+                case "d":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        writer.WriteValue("Divide");
+                        //Trace.WriteLine(String.Format("{0} / {1} = {2}", num1, num2, result));
+                    }
+                    break;
+                default:
+                    break;
+            }
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
+            return result;
+        }
+
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+    }
+}
